1681489ffa247aad0f7d827c137eb7832f0f0169
Vitor Coelho <vijcoelho@gmail.com>
2023-06-07 00:47:13 -0300

    Create ClassePessoa.py

---
+++ Classes/ClassePessoa.py
@@ -0,0 +1,11 @@
+class pessoa:
+    def __init__(self, nome='unknown', idade=0, peso='unknown', altura=0):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+        
+    def envelhecer(self):
+        
+        
+    
