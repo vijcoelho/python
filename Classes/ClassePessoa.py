1f86e817708bb5a7fd93737d5916985b09a27fca
Vitor Coelho <vijcoelho@gmail.com>
2023-06-08 23:15:07 -0300

    Update ClassePessoa.py

--- Classes/ClassePessoa.py
+++ Classes/ClassePessoa.py
@@ -1,11 +1,9 @@
-class pessoa:
-    def __init__(self, nome='unknown', idade=0, peso='unknown', altura=0):
+class Pessoa:
+    def __init__(self,nome,idade,peso, altura):
         self.nome = nome
         self.idade = idade
         self.peso = peso
         self.altura = altura
-        
+
     def envelhecer(self):
         
-        
-    
