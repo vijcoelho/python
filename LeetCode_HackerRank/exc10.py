54a0d8e3ea03a5161c2be02f480df87b49c2cbc7
Vitor Coelho <vijcoelho@gmail.com>
2024-05-10 19:45:01 -0300

    up

--- LeetCode_HackerRank/exc10.py
+++ LeetCode_HackerRank/exc10.py
@@ -2,10 +2,15 @@ def isAnagram(s: str, t: str) -> bool:
     if len(s) != len(t):
         return False
     
+    for i in s:
+        if i not in t:
+            return False
+        if s.index(i) != t.index(i):
+            return False
     
-
-
-s = "car"
-t = "rac"
+    return True
+        
+s = "rat"
+t = "car"
 
 print(isAnagram(s, t))
